1c949f84c94a3d1f109763e5ff7d1ccb9aed3681
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-07-26 00:17:24 +0300

    bt11

--- src/lt/baltictalents/exercises/bt11/Main.java
+++ src/lt/baltictalents/exercises/bt11/Main.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
         List<Integer> list1 = new ArrayList<>(  );
         list1.add( 1 );
         list1.add( 2 );
